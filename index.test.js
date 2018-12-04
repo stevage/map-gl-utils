b441ce4da036919bf7385b32a87b4452e2dde3d1
Steve Bennett <me@stevebennett.me>
2018-12-05 10:13:10 +1100

    More test cases

--- index.test.js
+++ index.test.js
@@ -1,22 +1,26 @@
 const utils = require('./index');
-test('Attaches to map object', () => {
+
+function mockMap() {
+    return {
+        setPaintProperty: jest.fn(),
+        setLayoutProperty: jest.fn()
+    };
+}
+test('Attaches itself to map object', () => {
     const map = {};
     const U = new utils(map);
     expect(map.U).toBe(U);    
 });
 
-test('Provides a function', () => {
+test('Provides hoverPointer function', () => {
     const map = {};
     const U = new utils(map);
     expect(typeof U.hoverPointer).toBe('function');    
 });
 
-describe('Setprop works', () => {
-    test('SetPaintProperty', () => {
-        const map = {
-            setPaintProperty: jest.fn(x => x),
-            setLayoutProperty: jest.fn(x => x)
-        };
+describe('setProp', () => {
+    test('Correctly picks setPaintProperty for line-color', () => {
+        const map = mockMap();
         const U = new utils(map);
         U.setProp('mylayer', 'lineColor', 'red');
         expect(map.setPaintProperty.mock.calls.length).toBe(1);
@@ -25,12 +29,8 @@ describe('Setprop works', () => {
         expect(map.setPaintProperty.mock.calls[0][2]).toBe('red');
         expect(map.setLayoutProperty.mock.calls.length).toBe(0);
     });
-    test('SetLayoutProperty', () => {
-        const map = {
-            setPaintProperty: jest.fn(x => x),
-            setLayoutProperty: jest.fn(x => x)
-        };
-    
+    test('Correctly picks setLayoutProperty for icon-size', () => {
+        const map = mockMap();    
         const U = new utils(map);
         U.setProp('mylayer', 'icon-size', 3);
         expect(map.setPaintProperty.mock.calls.length).toBe(0);
@@ -38,5 +38,45 @@ describe('Setprop works', () => {
         expect(map.setLayoutProperty.mock.calls[0][1]).toBe('icon-size');
         expect(map.setLayoutProperty.mock.calls[0][2]).toBe(3);
         expect(map.setLayoutProperty.mock.calls.length).toBe(1);
+    });
+    test('Handles multiple properties correctly', () => {
+        const map = mockMap();    
+        const U = new utils(map);
+        U.setProp('mylayer', {
+            'font-size': 12,
+            'text-color': 'blue',
+        });
+        expect(map.setPaintProperty.mock.calls.length).toBe(1);
+        expect(map.setPaintProperty.mock.calls[0][0]).toBe('mylayer');
+        expect(map.setPaintProperty.mock.calls[0][1]).toBe('text-color');
+        expect(map.setPaintProperty.mock.calls[0][2]).toBe('blue');
+        expect(map.setLayoutProperty.mock.calls.length).toBe(1);
+        expect(map.setLayoutProperty.mock.calls[0][0]).toBe('mylayer');
+        expect(map.setLayoutProperty.mock.calls[0][1]).toBe('font-size');
+        expect(map.setLayoutProperty.mock.calls[0][2]).toBe(12);
+    });
+    test('Supports multiple properties in camel case', () => {
+        const map = mockMap();    
+        const U = new utils(map);
+        U.setProp('mylayer', {
+            fontSize: 12,
+            textColor: 'blue',
+        });
+        expect(map.setPaintProperty.mock.calls.length).toBe(1);
+        expect(map.setPaintProperty.mock.calls[0][0]).toBe('mylayer');
+        expect(map.setPaintProperty.mock.calls[0][1]).toBe('text-color');
+        expect(map.setPaintProperty.mock.calls[0][2]).toBe('blue');
+        expect(map.setLayoutProperty.mock.calls.length).toBe(1);
+        expect(map.setLayoutProperty.mock.calls[0][0]).toBe('mylayer');
+        expect(map.setLayoutProperty.mock.calls[0][1]).toBe('font-size');
+        expect(map.setLayoutProperty.mock.calls[0][2]).toBe(12);
+    });
+    test('Supports a single property in camel case', () => {
+        const map = mockMap();    
+        const U = new utils(map);
+        U.setProp('mylayer', 'fontSize', 12);
+        expect(map.setLayoutProperty.mock.calls[0][0]).toBe('mylayer');
+        expect(map.setLayoutProperty.mock.calls[0][1]).toBe('font-size');
+        expect(map.setLayoutProperty.mock.calls[0][2]).toBe(12);
     });
 });
