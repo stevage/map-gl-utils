2921d0c6553e68fa6934d2832b645a0af39a8d35
Steve Bennett <stevage@gmail.com>
2020-02-17 10:56:59 +1100

    Rename update-keys

--- updateKeys.js
+++ updateKeys.js
@@ -3,20 +3,20 @@ Generates the list of paint and layout properties from the Mapbox-GL-JS style sp
 Should be run when new properties are added to that spec.
 */
 
-const v8 = require('@mapbox/mapbox-gl-style-spec/reference/v8.json');
+const styleSpec = require('@mapbox/mapbox-gl-style-spec/reference/v8.json');
 const fs = require('fs');
 const styleSpecVersion = require('./node_modules/@mapbox/mapbox-gl-style-spec/package.json').version;
 const out = {
     paints: [],
     layouts: []
 };
-Object.keys(v8)
+Object.keys(styleSpec)
     .filter(x => /^paint_/.test(x))
-    .forEach(key => out.paints.push(...Object.keys(v8[key])));
+    .forEach(key => out.paints.push(...Object.keys(styleSpec[key])));
 
-Object.keys(v8)
+Object.keys(styleSpec)
     .filter(x => /^layout_/.test(x))
-    .forEach(key => out.layouts.push(...Object.keys(v8[key])));
+    .forEach(key => out.layouts.push(...Object.keys(styleSpec[key])));
     
 out.paints = Array.from(new Set(out.paints));
 out.layouts = Array.from(new Set(out.layouts));
